38783e4855a59909bd99b4cf968c13fec1f0df14
Cristi <cristi@cristi-Satellite-L755D.(none)>
2013-11-08 17:10:38 -0800

    Adding remaining actions to posts controller. Also linking each post in the index to show and edit pages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,74 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
 
-  def show
-  	@post = Post.find(params[:id])
-  end
+before_action :set_post, only: [:show, :edit, :update]
+
+
+def index
+	@posts = Post.all
+end
+
+def show
+end
+
+def new
+	@post = Post.new
+end
+
+def create
+	@post = Post.new(post_params)
+
+	if @post.save
+		flash[:notice] = "Your post was successfully saved"
+		redirect_to posts_path
+	else
+		render :new
+	end
+end
+
+def edit
+end
+
+def update
+
+	if @post.update(post_params)
+		flash[:notice] = "Your post was successfully updated"
+		redirect_to posts_path
+	else
+		render :edit
+	end
+end
+
+private
+
+def post_params
+	params.require(:post).permit(:title, :url, :description)
+end
+
+def set_post
+	@post = Post.find(params[:id])
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Base
 
 	has_many :post_categories
 	has_many :categories, through: :post_categories
+
+	validates :title, presence: true
+	validates :url, presence: true
+	validates :description, presence: true
 end
