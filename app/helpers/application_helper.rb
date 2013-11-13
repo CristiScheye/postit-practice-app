60ca07bfc67cf8296fd127d1857447b76457aac5
Cristi Scheye <cristi.scheye@gmail.com>
2013-11-12 22:56:40 -0800

    Adding post partial. Adding helper methods for improved time formatting and correct urls.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def fix_url(str)
+		str.start_with?("http://") ? str : "http://#{str}"
+	end
+
+	def format_time(time)
+		time.strftime("on %A %b-%e-%Y at %l:%M%P")
+	end
 end
