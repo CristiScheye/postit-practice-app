163317eb6278c37e9014f138cfc6eb30dc28874a
Cristi Scheye <cristi.scheye@gmail.com>
2014-01-06 20:48:15 -0800

    Removing titleize from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,8 @@ class User < ActiveRecord::Base
 	validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3}
 	validates :password, presence: true, on: :create, length: {minimum: 5}
 
-  before_save :proper_case
   before_save :make_slug
 
-  def proper_case
-    self.username = self.username.titleize
-  end
 
   def make_slug
     self.slug = self.username.gsub(/[\s\W]/,'-').downcase
